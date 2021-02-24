6bc6bc8e6e9fce5027a22d84c9e25937fd881662
Diagorn <diagorn1999@yandex.ru>
2021-02-24 15:58:10 +0300

    Renewed the admin password

--- src/main/resources/db/migration/V2__Add_default_admin.sql
+++ src/main/resources/db/migration/V2__Add_default_admin.sql
@@ -1,5 +1,5 @@
 insert into usr (id, username, password, active, file_name, is_physical, person_id)
-    values (0, 'admin@admin.admin', '123', true, 'defaultavatar.png', true, 0);
+    values (0, 'admin@admin.admin', 'Shindina_2021', true, 'defaultavatar.png', true, 0);
 
 insert into user_role (user_id, roles)
     values (0, 'USER'), (0, 'ADMIN');
